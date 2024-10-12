0f8490fedc22dfa8a51272485c99d3238d42f6a6
nwakakukaks <winnernwakaku123@gmail.com>
2024-10-12 22:00:46 +0100

    finished UI

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,11 +1,34 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
-import { Avatar, AvatarFallback, AvatarImage } from "./components/ui/avatar";
-import { LucideCheck, Star } from "lucide-react";
+import { Card, CardContent } from "@/components/ui/card";
+import { CircleArrowDown, CircleArrowOutUpRight, Minus, Plus, Star } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import { Separator } from "./components/ui/separator";
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible";
+import { useState } from "react";
 
 const App = () => {
   const navigate = useNavigate();
+  const [openIndex, setOpenIndex] = useState(null);
+
+  const faqs = [
+    {
+      question: "Can I use this in my project?",
+      answer: "Yes. Free to use for personal and commercial projects. No attribution required.",
+    },
+    {
+      question: "Is there any support available?",
+      answer: "Yes, support is available via our documentation and community forums.",
+    },
+    {
+      question: "How do I report a bug?",
+      answer: "You can report bugs through our GitHub issue tracker.",
+    },
+    // Add more FAQs as needed
+  ];
+
+  const toggleFAQ = (index: any) => {
+    setOpenIndex(openIndex === index ? null : index);
+  };
 
   return (
     <div className="min-h-screen bg-white">
@@ -88,13 +111,13 @@ const App = () => {
                     <p className="text-green font-semibold text-sm">5.0</p>
                   </div>
 
-                  <p className="text-green text-xs">Trusted by 200+ individuals</p>
+                  <p className="text-green text-xs">Trusted by 800+ individuals</p>
                 </div>
               </div>
             </div>
             <div className="w-full md:w-1/2">
               <img
-                src="https://res.cloudinary.com/dgbreoalg/image/upload/v1728636643/happy-men-bachelor-party_gngtcl.png"
+                src="https://utfs.io/f/PKy8oE1GN2J3A6pNlecTLpFHQKPUGyM3O2Id90ntzxhmiAsV"
                 alt="Digital Asset Security"
                 className="rounded-lg shadow-lg w-full"
               />
@@ -102,8 +125,108 @@ const App = () => {
           </div>
         </section>
 
-        {/* Features Section */}
-        <section id="features" className="py-20">
+        {/* Partners and companies */}
+        <section>
+          <div className="space-y-3">
+            <Separator />
+            <div className="flex justify-evenly items-center">
+              <p className="ml-2 flex items-center text-green font-semibold">
+                {" "}
+                <img src="/p1.svg" className="w-12 h-12" /> Eternity
+              </p>
+
+              <p className="ml-2 flex items-center text-green font-semibold">
+                {" "}
+                <img src="/p3.svg" className="w-16 h-16" />
+              </p>
+              <p className="ml-2 flex items-center text-green font-semibold">
+                {" "}
+                <img src="/p4.svg" className="w-9 h-9" /> Gnosis
+              </p>
+              <p className="ml-2 flex items-center text-green font-semibold">
+                {" "}
+                <img src="/p5.svg" className="w-8 h-8" /> Celo
+              </p>
+              <p className="ml-2 flex items-center text-green font-semibold">
+                {" "}
+                <img src="/p2.svg" className="w-14 h-14" />
+              </p>
+            </div>
+            <Separator />
+          </div>
+        </section>
+
+        {/* Features us */}
+        <section className="p-10 space-y-4">
+          <div className="flex justify-between items-end">
+            <div>
+              <p className="text-green text-xs">VALUES!</p>
+              <h2 className="text-2xl font-semibold text-navy">
+                Keep your assets
+                <br />
+                <span className="">Well-secured</span>
+              </h2>
+            </div>
+
+            <p className="text-green text-xs line-clamp-2 max-w-96">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus nemo aut explicabo omnis quisquam
+              veritatis provident, sed facilis dolor laudantium quas sunt. At soluta quasi mollitia provident doloremque
+              sunt autem alias voluptas commodi iste debitis animi tempore eveniet unde quibusdam magnam odit enim
+              repellendus saepe magni nisi, harum dolorem dignissimos.
+            </p>
+          </div>
+
+          <div className="flex space-x-3 items-center">
+            <Card className="bg-transparent rounded-none border border-gray-500 py-6">
+              <CardContent className="flex flex-col gap-6">
+                <CircleArrowDown />
+
+                <div className="space-y-2">
+                  <p className="font-semibold text-navy">Transparency</p>
+                  <p className="text-sm text-green">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit eos voluptatibus unde cum hic
+                    nesciunt?
+                  </p>
+                </div>
+
+                <CircleArrowOutUpRight />
+              </CardContent>
+            </Card>
+            <Card className="bg-transparent rounded-none border border-gray-500 py-6">
+              <CardContent className="flex flex-col gap-6">
+                <CircleArrowDown />
+
+                <div className="space-y-2">
+                  <p className="font-semibold text-navy">Transparency</p>
+                  <p className="text-sm text-green">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit eos voluptatibus unde cum hic
+                    nesciunt?
+                  </p>
+                </div>
+
+                <CircleArrowOutUpRight />
+              </CardContent>
+            </Card>
+            <Card className="bg-carton rounded-none rounded-tr-[80px] border border-gray-500 py-6">
+              <CardContent className="flex flex-col gap-6">
+                <CircleArrowDown />
+
+                <div className="space-y-2">
+                  <p className="font-semibold text-navy">Transparency</p>
+                  <p className="text-sm text-green">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit eos voluptatibus unde cum hic
+                    nesciunt?
+                  </p>
+                </div>
+
+                <CircleArrowOutUpRight className="bg-green p-1 rounded-full" />
+              </CardContent>
+            </Card>
+          </div>
+        </section>
+
+        {/* Features */}
+        {/* <section id="features" className="py-20">
           <div className="container mx-auto px-8">
             <h2 className="text-3xl font-bold text-center mb-12">KEY FEATURES</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
@@ -154,132 +277,64 @@ const App = () => {
               </Card>
             </div>
           </div>
-        </section>
+        </section> */}
 
-        {/* How It Works Section */}
-        <section id="how-it-works" className="bg-slate-600 border border-gray-600 text-white py-20">
-          <div className="container mx-auto px-8">
-            <h2 className="text-3xl font-bold text-center text-white mb-12">HOW IT WORKS</h2>
-            <div className="flex flex-col md:flex-row items-center px-8">
-              <div className="w-full md:w-1/2 mb-8 md:mb-0">
-                <ul className=" space-y-8 text-lg">
-                  <li className="flex items-center gap-2">
-                    <span className="p-2 rounded-full border-[#6EE7B7] border-2 ">
-                      <LucideCheck />
-                    </span>
-                    <p>Sign up and verify your identity</p>
-                  </li>
-                  <li className="flex items-center gap-2">
-                    <span className="p-2 rounded-full border-[#6EE7B7] border-2 ">
-                      <LucideCheck />
-                    </span>
-
-                    <p>Add your digital assets to the system</p>
-                  </li>
-                  <li className="flex items-center gap-2">
-                    <span className="p-2 rounded-full border-[#6EE7B7] border-2">
-                      <LucideCheck />
-                    </span>
-                    <p>Set up beneficiaries and distribution rules</p>
-                  </li>
-                  <li className="flex items-center gap-2">
-                    <span className="p-2 rounded-full border-[#6EE7B7] border-2">
-                      <LucideCheck />
-                    </span>
-                    <p>Specify trigger conditions or dates</p>
-                  </li>
-                  <li className="flex items-center gap-2">
-                    <span className="p-2 rounded-full border-[#6EE7B7] border-2">
-                      <LucideCheck />
-                    </span>
-                    <p>Rest easy knowing your digital legacy is secure</p>
-                  </li>
-                </ul>
-              </div>
-              <div className="w-full flex items-center justify-center md:w-1/2">
-                <img
-                  src="https://res.cloudinary.com/dgbreoalg/image/upload/v1728554048/ella_ckh7pt.jpg"
-                  alt="dd"
-                  className="rounded-sm"
-                />
-              </div>
+        {/* Stats section */}
+        <section id="stats" className="bg-green text-white p-10">
+          <div className="flex justify-between items-center">
+            <div className="space-y-2">
+              <p className="text-4xl font-bold text-center">$500M </p>
+              <p className="text-base">Total assets and funds locked </p>
+            </div>
+            <div className="space-y-2">
+              <p className="text-4xl font-bold text-center">23k + </p>
+              <p className="text-base">Disbursed per day </p>
+            </div>
+            <div className="space-y-2">
+              <p className="text-4xl font-bold text-center">12+ </p>
+              <p className="text-base">Ecosystem partners </p>
             </div>
           </div>
         </section>
 
         {/* Testimonials Section */}
-        <section id="testimonials" className="py-20">
-          <div className="container mx-auto lg:px-24">
-            <h2 className="text-3xl font-bold text-center mb-12">WHAT OUR USERS SAY</h2>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-              <Card className=" items-start">
-                <CardHeader className="flex flex-row items-center gap-1">
-                  <Avatar>
-                    <AvatarImage src="https://xsgames.co/randomusers/avatar.php?g=male" alt="@shadcn" />
-                    <AvatarFallback>CN</AvatarFallback>
-                  </Avatar>
-                  <div>
-                    <CardTitle>John D.</CardTitle>
-                    <CardDescription>Crypto Investor</CardDescription>
-                  </div>
-                </CardHeader>
-                <CardContent>
-                  <p>
-                    "AssetGuard gives me peace of mind knowing my digital assets are secure and will be distributed
-                    according to my wishes."
-                  </p>
-                </CardContent>
-              </Card>
-              <Card>
-                <CardHeader className="flex flex-row items-center gap-1">
-                  <Avatar>
-                    <AvatarImage src="https://xsgames.co/randomusers/assets/avatars/female/48.jpg" alt="@shadcn" />
-                    <AvatarFallback>CN</AvatarFallback>
-                  </Avatar>
-                  <div>
-                    <CardTitle>Sarah M.</CardTitle>
-                    <CardDescription>Tech Investor</CardDescription>
-                  </div>
-                </CardHeader>
-                <CardContent>
-                  <p>
-                    "The user-friendly interface and robust security features make AssetGuard the perfect solution for
-                    managing my digital legacy."
-                  </p>
-                </CardContent>
-              </Card>
-              <Card>
-                <CardHeader className="flex flex-row items-center gap-1">
-                  <Avatar>
-                    <AvatarImage src="https://xsgames.co/randomusers/assets/avatars/male/27.jpg" alt="@shadcn" />
-                    <AvatarFallback>CN</AvatarFallback>
-                  </Avatar>
-                  <div>
-                    <CardTitle>Moses A.</CardTitle>
-                    <CardDescription>Tech Entrepreneur</CardDescription>
-                  </div>
-                </CardHeader>
-                <CardContent>
-                  <p>
-                    "The user-friendly interface and robust security features make AssetGuard the perfect solution for
-                    managing my digital legacy."
-                  </p>
-                </CardContent>
-              </Card>
-            </div>
+        <section id="faq" className="p-10 flex justify-between space-x-10">
+          <div className="flex-shrink-0">
+            <p className="text-sm text-green">FAQ</p>
+            <p className="text-2xl text-navy font-semibold">Frequently asked questions</p>
+          </div>
+
+          <div className="w-1/2">
+            {" "}
+            {/* Set a fixed width or use a fraction */}
+            {faqs.map((faq, index) => (
+              <div key={index} className="mb-4">
+                <Collapsible open={openIndex === index} onOpenChange={() => toggleFAQ(index)}>
+                  <CollapsibleTrigger className="flex items-center justify-between w-full">
+                    <span className="text-sm text-navy">{faq.question}</span>
+                    {openIndex === index ? <Minus className="ml-2 w-4 h-4" /> : <Plus className="ml-2 w-4 h-4" />}
+                  </CollapsibleTrigger>
+                  <CollapsibleContent>
+                    <p className="text-xs mt-3">{faq.answer}</p>
+                  </CollapsibleContent>
+                </Collapsible>
+                <div className="border-b border-gray-300 my-5"></div> {/* Separator */}
+              </div>
+            ))}
           </div>
         </section>
       </main>
 
       {/* Footer */}
-      <footer className="bg-slate-900 text-white py-8">
+
+      <footer className=" text-navy py-8">
         <div className="container mx-auto px-8">
           <div className="flex flex-col sm:flex-row justify-between items-center">
             <div className="mb-4 sm:mb-0">
-              <h3 className="text-xl font-bold">AssetGuard</h3>
-              <p>Securing your digital future</p>
+              <h3 className="text-lg font-semibold text-green">AssetGuard</h3>
+              <p className="text-sm">Securing your digital future</p>
             </div>
-            <nav className="mb-4 sm:mb-0">
+            <nav className="mb-4 sm:mb-0 text-sm">
               <ul className="flex flex-wrap justify-center sm:justify-end space-x-4">
                 <li>
                   <a href="#" className="hover:text-blue-400">
@@ -299,7 +354,7 @@ const App = () => {
               </ul>
             </nav>
           </div>
-          <div className="mt-4 text-center">
+          <div className="mt-4 text-center text-sm">
             <p>&copy; 2024 AssetGuard. All rights reserved.</p>
           </div>
         </div>
